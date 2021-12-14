b571c09a15cc6ee06e8530041e139ee76ce064cc
WiljamiT <74732018+WiljamiT@users.noreply.github.com>
2021-12-14 23:59:36 +0200

    jotain

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
+app.use(express.static("public"));
 // yhteys eri porteista ja domaineista
 app.use(cors());
 app.use(express.json()); // muuttaa json stringin json objektiksi (pyynnöstä)
